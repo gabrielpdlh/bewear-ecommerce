376ce23c47155688f0a38330b18642364e102ddb
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-11-17 20:57:00 -0300

    chore: add shipping address and cart schemas

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -15,6 +15,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import Cart from "./cart";
 
 const Header = () => {
   const { data: session } = authClient.useSession();
@@ -25,7 +26,7 @@ const Header = () => {
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-4">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline">
@@ -82,6 +83,7 @@ const Header = () => {
             </div>
           </SheetContent>
         </Sheet>
+        <Cart />
       </div>
     </header>
   );
