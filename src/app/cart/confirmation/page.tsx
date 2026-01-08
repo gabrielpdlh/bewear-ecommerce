16f6e7ea0690f57dee8e550eb053a438ba5b0f6f
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2026-01-07 22:10:45 -0300

    feat: ensure order can be created

--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -1,23 +1,21 @@
-import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/db";
-import { shippingAddressTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import CartSummary from "../components/cart-summary";
 import { formatAddress } from "../helpers/address";
+import FinishOrderButton from "./finish-order-button";
 
 const ConfirmationPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (!session) {
+  if (!session?.user.id) {
     redirect("/");
   }
   const cart = await db.query.cartTable.findFirst({
@@ -38,34 +36,42 @@ const ConfirmationPage = async () => {
   if (!cart || cart?.items.length === 0) {
     redirect("/");
   }
-  const shippingAddresses = await db.query.shippingAddressTable.findMany({
-    where: eq(shippingAddressTable.userId, session.user.id),
-  });
+  const cartTotalInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
+  if (!cart.shippingAddress) {
+    redirect("/cart/identification");
+  }
   return (
     <div>
       <Header />
       <div className="space-y-4 px-5">
         <Card>
           <CardHeader>
-            <CardTitle>Indetificação</CardTitle>
+            <CardTitle>Identificação</CardTitle>
           </CardHeader>
           <CardContent className="space-y-6">
             <Card>
               <CardContent>
-                <p>
-                  {cart.shippingAddress
-                    ? formatAddress(cart.shippingAddress)
-                    : "Endereço não informado"}
-                </p>
+                <p className="text-sm">{formatAddress(cart.shippingAddress)}</p>
               </CardContent>
             </Card>
-            <Button className="w-full rounded-full" size="lg">
-              Finalizar Compra
-            </Button>
+            <FinishOrderButton />
           </CardContent>
         </Card>
-        <h1>Confirmação</h1>
-        <CartSummary />
+        <CartSummary
+          subtotalInCents={cartTotalInCents}
+          totalInCents={cartTotalInCents}
+          products={cart.items.map((item) => ({
+            id: item.productVariant.id,
+            name: item.productVariant.product.name,
+            variantName: item.productVariant.name,
+            quantity: item.quantity,
+            priceInCents: item.productVariant.priceInCents,
+            imageUrl: item.productVariant.imageUrl,
+          }))}
+        />
       </div>
       <div className="mt-12">
         <Footer />
--- src/hooks/queries/use-cart.ts
+++ src/hooks/queries/use-cart.ts
@@ -2,13 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getCart } from "@/actions/get-cart";
 
-export const getCartQueryKey = () => ["cart"] as const;
+export const getUseCartQueryKey = () => ["cart"] as const;
 
 export const useCart = (params?: {
   initialData?: Awaited<ReturnType<typeof getCart>>;
 }) => {
   return useQuery({
-    queryKey: getCartQueryKey(),
+    queryKey: getUseCartQueryKey(),
     queryFn: () => getCart(),
     initialData: params?.initialData,
   });
